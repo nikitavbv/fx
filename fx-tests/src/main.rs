c30aad36e2d9701c8ba788a8aa45439be982ab9d
Nikita Volobuev <nikitavbv@gmail.com>
2025-05-01 17:28:32 +0200

    add test for ServiceNotFound

--- fx-tests/src/main.rs
+++ fx-tests/src/main.rs
@@ -26,8 +26,8 @@ fn main() {
     test_sql_simple(&fx);
     test_sqlx(&fx);
     test_invoke_function_non_existent(&fx);
+    test_invoke_function_non_existent_rpc(&fx);
     test_invoke_function_no_module_code(&fx);
-    // TODO: test what happens if you invoke function that does not exist
     // TODO: test what happens if you invoke function with wrong argument
     // TODO: test what happens if function panics
     // TODO: test that database can only be accessed by correct binding name
@@ -58,6 +58,12 @@ fn test_sqlx(fx: &FxCloud) {
 
 fn test_invoke_function_non_existent(fx: &FxCloud) {
     println!("> test_invoke_function_non_existent");
+    let result = fx.invoke_service::<(), ()>(&ServiceId::new("test-non-existent".to_owned()), "simple", ());
+    assert_eq!(Err(FxCloudError::ServiceNotFound), result);
+}
+
+fn test_invoke_function_non_existent_rpc(fx: &FxCloud) {
+    println!("> test_invoke_function_non_existent_rpc");
     let result = fx.invoke_service::<(), ()>(&ServiceId::new("test-app".to_owned()), "function_non_existent", ());
     assert_eq!(Err(FxCloudError::RpcHandlerNotDefined), result);
 }
