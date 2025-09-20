237ed3e5168c053f82f9a8b58f1ef006ee32cc38
Nikita Volobuev <nikitavbv@gmail.com>
2025-09-20 23:41:27 +0200

    api: return error if rpc binding not found

--- fx-runtime/src/api/rpc.rs
+++ fx-runtime/src/api/rpc.rs
@@ -1,6 +1,6 @@
 use {
     wasmer::FunctionEnvMut,
-    tracing::{error, warn},
+    tracing::error,
     fx_common::FxFutureError,
     futures::FutureExt,
     crate::{
@@ -8,28 +8,39 @@ use {
     },
 };
 
+#[derive(Clone, Copy)]
+pub enum ResultCode {
+    BindingNotFound = -1,
+    RuntimeError = -2,
+}
+
+impl ResultCode {
+    fn as_i64(&self) -> i64 {
+        *self as i64
+    }
+}
+
 pub fn handle_rpc(
     ctx: FunctionEnvMut<ExecutionEnv>,
-    service_name_ptr: i64,
-    service_name_len: i64,
-    function_name_ptr: i64,
-    function_name_len: i64,
+    function_id_ptr: i64,
+    function_id_len: i64,
+    method_name_ptr: i64,
+    method_name_len: i64,
     arg_ptr: i64,
     arg_len: i64,
 ) -> i64 {
-    let service_id = FunctionId::new(String::from_utf8(read_memory_owned(&ctx, service_name_ptr, service_name_len)).unwrap());
+    let function_id = FunctionId::new(String::from_utf8(read_memory_owned(&ctx, function_id_ptr, function_id_len)).unwrap());
 
-    let binding = ctx.data().rpc.get(&service_id.as_string());
+    let binding = ctx.data().rpc.get(&function_id.as_string());
     if binding.is_none() {
-        let this_function_name = ctx.data().service_id.as_string();
-        warn!("function {this_function_name:?} does not have an rpc binding defined for {:?} which it calls. This will be a runtime error in a next version of fx.", service_id.as_string());
+        return ResultCode::BindingNotFound.as_i64();
     }
 
-    let function_name = String::from_utf8(read_memory_owned(&ctx, function_name_ptr, function_name_len)).unwrap();
+    let method_name = String::from_utf8(read_memory_owned(&ctx, method_name_ptr, method_name_len)).unwrap();
     let argument = read_memory_owned(&ctx, arg_ptr, arg_len);
 
     let engine = ctx.data().engine.clone();
-    let response_future = match engine.clone().invoke_service_raw(engine.clone(), service_id, function_name, argument) {
+    let response_future = match engine.clone().invoke_service_raw(engine.clone(), function_id, method_name, argument) {
         Ok(response_future) => response_future.map(|v| v.map_err(|err| FxFutureError::RpcError {
             reason: err.to_string(),
         })).boxed(),
@@ -39,8 +50,7 @@ pub fn handle_rpc(
         Ok(v) => v,
         Err(err) => {
             error!("failed to push future to futures arena: {err:?}");
-            // todo: write error object
-            return -1;
+            return ResultCode::RuntimeError.as_i64();
         }
     };
 
