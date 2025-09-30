f62ab23891039ab2392318a8191f6af5ad6d53ae
Nikita Volobuev <nikitavbv@gmail.com>
2025-09-30 22:34:28 +0200

    refactor: move send_rpc_response to api module

--- fx-runtime/src/runtime.rs
+++ fx-runtime/src/runtime.rs
@@ -546,7 +546,7 @@ impl ExecutionContext {
         let mut import_object = imports! {
             "fx" => {
                 "rpc" => Function::new_typed_with_env(&mut store, &function_env, crate::api::rpc::handle_rpc),
-                "send_rpc_response" => Function::new_typed_with_env(&mut store, &function_env, api_send_rpc_response),
+                "send_rpc_response" => Function::new_typed_with_env(&mut store, &function_env, crate::api::rpc::handle_send_rpc_response),
                 "send_error" => Function::new_typed_with_env(&mut store, &function_env, api_send_error),
                 "kv_get" => Function::new_typed_with_env(&mut store, &function_env, crate::api::kv::handle_kv_get),
                 "kv_set" => Function::new_typed_with_env(&mut store, &function_env, crate::api::kv::handle_kv_set),
@@ -687,10 +687,6 @@ fn decode_memory<T: serde::de::DeserializeOwned>(ctx: &FunctionEnvMut<ExecutionE
     let memory = read_memory_owned(&ctx, addr, len);
     rmp_serde::from_slice(&memory)
         .map_err(|err| FxRuntimeError::SerializationError { reason: format!("failed to decode memory: {err:?}") })
-}
-
-fn api_send_rpc_response(mut ctx: FunctionEnvMut<ExecutionEnv>, addr: i64, len: i64) {
-    ctx.data_mut().rpc_response = Some(read_memory_owned(&ctx, addr, len));
 }
 
 fn api_send_error(mut ctx: FunctionEnvMut<ExecutionEnv>, addr: i64, len: i64) {
