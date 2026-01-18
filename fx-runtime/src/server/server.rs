95047d287e29afea45a43dc470ae5ad6d9dcaaf3
Nikita Volobuev <nikitavbv@gmail.com>
2026-01-18 18:46:38 +0100

    server: lazy http server start

--- fx-runtime/src/server/server.rs
+++ fx-runtime/src/server/server.rs
@@ -1,7 +1,10 @@
 use {
-    std::{sync::Arc, path::PathBuf, collections::HashMap},
-    tokio::sync::{Mutex, RwLock},
-    tracing::{info, warn},
+    std::{sync::Arc, path::PathBuf, collections::HashMap, net::SocketAddr},
+    tokio::{
+        sync::{Mutex, RwLock, mpsc},
+        net::TcpListener,
+    },
+    tracing::{info, warn, error},
     walkdir::WalkDir,
     crate::{
         runtime::{FxRuntime, FunctionId},
@@ -17,56 +20,111 @@ const EMPTY_FUNCTION_CONFIG: FunctionConfig = FunctionConfig {
 pub struct FxServer {
     runtime: Arc<FxRuntime>,
     definitions_monitor: DefinitionsMonitor,
-    listener_definitions: Arc<RwLock<ListenerDefinitions>>,
+
+    http_definition_rx: Arc<Mutex<mpsc::Receiver<HttpListenerDefinition>>>,
 }
 
 struct DefinitionsMonitor {
     runtime: Arc<FxRuntime>,
-    listener_definitions: Arc<RwLock<ListenerDefinitions>>,
     functions_directory: PathBuf,
     functions_configs: Arc<Mutex<HashMap<FunctionId, FunctionConfig>>>,
+
+    http_definition_tx: Arc<Mutex<mpsc::Sender<HttpListenerDefinition>>>,
 }
 
-struct ListenerDefinitions {
-    http: Option<Arc<HttpListenerDefinition>>,
-}
-
+#[derive(Clone)]
 struct HttpListenerDefinition {
-    function_id: FunctionId,
+    function: Option<FunctionId>,
+}
+
+struct DefinitionSubscribers {
+    http: mpsc::Receiver<HttpListenerDefinition>,
 }
 
 impl FxServer {
     pub fn new(config: ServerConfig, runtime: FxRuntime) -> Self {
         let runtime = Arc::new(runtime);
-        let listener_definitions = Arc::new(RwLock::new(ListenerDefinitions::new()));
-        let definitions_monitor = DefinitionsMonitor::new(runtime.clone(), listener_definitions.clone(), &config);
+
+        let (http_tx, http_rx) = mpsc::channel::<HttpListenerDefinition>(100);
+
+        let definitions_monitor = DefinitionsMonitor::new(
+            runtime.clone(),
+            &config,
+            http_tx,
+        );
 
         Self {
             runtime,
             definitions_monitor,
-            listener_definitions,
+
+            http_definition_rx: Arc::new(Mutex::new(http_rx)),
         }
     }
 
     pub async fn serve(&self) {
-        self.definitions_monitor.scan_definitions().await;
+        tokio::join!(
+            self.definitions_monitor.scan_definitions(),
+            self.run_http_listener()
+        );
+    }
 
-        unimplemented!("serving stuff is not implemented yet")
+    async fn run_http_listener(&self) {
+        let mut http_definition_tx = self.http_definition_rx.lock().await;
+        while let Some(mut definition) = http_definition_tx.recv().await {
+            let server_function = match definition.function {
+                Some(v) => v,
+                None => {
+                    // target function not defined, continue sleeping...
+                    continue;
+                }
+            };
+
+            let addr: SocketAddr = ([0, 0, 0, 0], 8080).into();
+            let listener = match TcpListener::bind(addr).await {
+                Ok(v) => v,
+                Err(err) => {
+                    error!("failed to bind tcp listener for http sever: {err:?}");
+                    continue;
+                }
+            };
+
+            info!("started http server on {addr:?}");
+            loop {
+                tokio::select! {
+                    new_definition = http_definition_tx.recv() => {
+                        panic!("new definition");
+                    },
+                    connection = listener.accept() => {
+                        panic!("new connection")
+                    }
+                }
+            }
+
+            unimplemented!("oops, time to start http server: {}", server_function.as_string());
+        }
     }
 }
 
 impl DefinitionsMonitor {
-    pub fn new(runtime: Arc<FxRuntime>, listener_definitions: Arc<RwLock<ListenerDefinitions>>, config: &ServerConfig) -> Self {
+    pub fn new(
+        runtime: Arc<FxRuntime>,
+        config: &ServerConfig,
+        http_definition_tx: mpsc::Sender<HttpListenerDefinition>,
+    ) -> Self {
         Self {
             runtime,
-            listener_definitions,
             functions_directory: config.config_path.as_ref().unwrap().parent().unwrap().join(&config.functions_dir),
             functions_configs: Arc::new(Mutex::new(HashMap::new())),
+            http_definition_tx: Arc::new(Mutex::new(http_definition_tx)),
         }
     }
 
     pub async fn scan_definitions(&self) {
         info!("will scan definitions in {:?}", self.functions_directory);
+
+        let mut definition_http = HttpListenerDefinition {
+            function: None,
+        };
 
         let root = &self.functions_directory;
         for entry in WalkDir::new(root) {
@@ -94,22 +152,62 @@ impl DefinitionsMonitor {
             let function_config = FunctionConfig::load(entry_path).await;
             let prev_function_config = self.functions_configs.lock().await.get(&function_id).unwrap_or(&EMPTY_FUNCTION_CONFIG).clone();
 
-            self.apply_config(function_id, prev_function_config, function_config);
+            self.apply_config(
+                function_id,
+                prev_function_config,
+                function_config,
+                &mut definition_http,
+            ).await;
         }
 
-        unimplemented!("definitions scanning is not implemented yet")
+        // TODO: listen on fs changes
     }
 
-    fn apply_config(&self, function_id: FunctionId, prev_config: FunctionConfig, new_config: FunctionConfig) {
+    async fn apply_config(
+        &self,
+        function_id: FunctionId,
+        prev_config: FunctionConfig,
+        new_config: FunctionConfig,
+        definition_http: &mut HttpListenerDefinition,
+    ) {
         info!("applying config for {:?}", function_id.as_string());
-        // TODO: update definitions and send event
-    }
-}
 
-impl ListenerDefinitions {
-    fn new() -> Self {
-        Self {
-            http: None,
+        fn extract_http(config: &FunctionConfig) -> bool {
+            config.triggers.as_ref()
+                .map(|triggers| !triggers.http.is_empty())
+                .unwrap_or(false)
         }
+
+        let prev_http = extract_http(&prev_config);
+        let new_http = extract_http(&new_config);
+
+        if prev_http != new_http {
+            if new_http {
+                if let Some(existing_handler) = definition_http.function.as_ref() {
+                    if existing_handler != &function_id {
+                        panic!("http listener already set to a different function: {:?}", existing_handler.as_string());
+                    } else {
+                        // same listener as current, nothing to do
+                    }
+                } else {
+                    // no http listener configured, let's set one
+                    definition_http.function = Some(function_id.clone());
+                    self.http_definition_tx.lock().await.send(definition_http.clone()).await.unwrap();
+                }
+            } else {
+                if let Some(existing_handler) = definition_http.function.as_ref() {
+                    if existing_handler != &function_id {
+                        // existing handler set to a different function, nothing to do
+                    } else {
+                        definition_http.function = None;
+                        self.http_definition_tx.lock().await.send(definition_http.clone()).await.unwrap();
+                    }
+                } else {
+                    // no http listener configured, nothing to do
+                }
+            }
+        }
+
+        // TODO: update definitions and send event
     }
 }
