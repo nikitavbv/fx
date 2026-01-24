7d756fb74d819e5a7ed6ca49ae93d8bcee9461c8
Nikita Volobuev <nikitavbv@gmail.com>
2026-01-24 16:40:00 +0100

    server: load default config if config file is not present

--- fx-runtime/src/server/server.rs
+++ fx-runtime/src/server/server.rs
@@ -198,11 +198,6 @@ impl DefinitionsMonitor {
 
             let entry_path = entry.path();
             let function_id = self.path_to_function_id(entry_path);
-
-            if !fs::try_exists(&entry_path).await.unwrap() {
-                warn!("config for function {function_id:?} does not exist");
-                continue;
-            }
             let function_config = match FunctionConfig::load(entry_path.to_path_buf()).await {
                 Ok(v) => v,
                 Err(err) => {
@@ -249,7 +244,8 @@ impl DefinitionsMonitor {
             }
 
             let function_id = self.path_to_function_id(&path);
-            if !fs::try_exists(&path).await.unwrap() {
+
+            if !path.exists() {
                 self.remove_function(function_id, &mut definition_http).await;
                 continue;
             }
