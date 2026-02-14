33cce20b0ce05513bcb442a98bf8216be41d3142
Nikita Volobuev <nikitavbv@gmail.com>
2026-02-14 22:40:13 +0100

    v2: implement fetch in sdk

--- fx-sdk/src/api/http.rs
+++ fx-sdk/src/api/http.rs
@@ -1,8 +1,17 @@
 use {
     std::str::FromStr,
     http::{Uri, Method},
-    fx_types::{capnp, abi_host_resources_capnp},
-    crate::sys::{ResourceId, DeserializableHostResource, DeserializeHostResource},
+    fx_types::{capnp, abi_host_resources_capnp, abi_http_capnp},
+    crate::sys::{
+        ResourceId,
+        DeserializableHostResource,
+        DeserializeHostResource,
+        SerializeResource,
+        SerializableResource,
+        OwnedResourceId,
+        FutureHostResource,
+        fx_fetch,
+    },
 };
 
 pub struct HttpRequest(HttpRequestInner);
@@ -38,7 +47,7 @@ impl HttpRequest {
     }
 }
 
-struct HttpRequestData {
+pub(crate) struct HttpRequestData {
     method: Method,
     url: Uri,
 }
@@ -68,8 +77,41 @@ impl DeserializeHostResource for HttpRequestData {
     }
 }
 
-pub struct HttpResponse;
+pub struct HttpResponse {
+    status: http::StatusCode,
+    body: Vec<u8>,
+}
 
 pub async fn fetch(request: HttpRequest) -> HttpResponse {
-    todo!()
+    let mut message = capnp::message::Builder::new_default();
+    let mut fetch = message.init_root::<abi_http_capnp::http_request::Builder>();
+    fetch.set_method(match request.method() {
+        &Method::GET => abi_http_capnp::HttpMethod::Get,
+        &Method::DELETE => abi_http_capnp::HttpMethod::Delete,
+        &Method::OPTIONS => abi_http_capnp::HttpMethod::Options,
+        &Method::PATCH => abi_http_capnp::HttpMethod::Patch,
+        &Method::POST => abi_http_capnp::HttpMethod::Post,
+        &Method::PUT => abi_http_capnp::HttpMethod::Put,
+        other => todo!("http method not supported: {other:?}"),
+    });
+    fetch.set_uri(request.uri().to_string());
+    let fetch = capnp::serialize::write_message_to_words(&message);
+
+    let response_resource = OwnedResourceId::from_ffi(
+        unsafe { fx_fetch(fetch.as_ptr() as u64, fetch.len() as u64) }
+    );
+
+    FutureHostResource::<HttpResponse>::new(response_resource).await
+}
+
+impl DeserializeHostResource for HttpResponse {
+    fn deserialize(data: &mut &[u8]) -> Self {
+        let resource_reader = capnp::serialize::read_message_from_flat_slice(data, capnp::message::ReaderOptions::default()).unwrap();
+        let request = resource_reader.get_root::<fx_types::abi_http_capnp::http_response::Reader>().unwrap();
+
+        HttpResponse {
+            status: http::StatusCode::from_u16(request.get_status()).unwrap(),
+            body: request.get_body().unwrap().to_vec(),
+        }
+    }
 }
