e53607804efec4645629bb158bfaa666a79f95d5
Nikita Volobuev <nikitavbv@gmail.com>
2026-05-31 17:07:55 +0200

    fx-website: add headers to test post endpoint

--- apps/fx-website/src/test_endpoints.rs
+++ apps/fx-website/src/test_endpoints.rs
@@ -1,17 +1,24 @@
 use {
+    std::collections::HashMap,
     serde::Serialize,
-    axum::{Json, http::Uri},
+    axum::{Json, http::{Uri, HeaderMap}},
 };
 
 #[derive(Serialize)]
 pub(crate) struct PostEndpointResponse {
     data: String,
+    headers: HashMap<String, String>,
     url: String,
 }
 
-pub(crate) async fn post(url: Uri, body: String) -> Json<PostEndpointResponse> {
+pub(crate) async fn post(url: Uri, headers: HeaderMap, body: String) -> Json<PostEndpointResponse> {
     Json(PostEndpointResponse {
         data: body,
+        headers: headers.into_iter()
+            .filter_map(|(header_name, header_value)| header_name.map(|header_name| {
+                (header_name.to_string(), header_value.to_str().unwrap().to_owned())
+            }))
+            .collect(),
         url: url.to_string(),
     })
 }
