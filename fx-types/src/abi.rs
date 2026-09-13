dc6a8748b32fbb67937b586c58abc03a7da38c6c
Nikita Volobuev <nikitavbv@gmail.com>
2026-09-13 23:25:47 +0200

    abi: add FetchResultPollResultCode type

--- fx-types/src/abi.rs
+++ fx-types/src/abi.rs
@@ -299,6 +299,15 @@ pub enum AbiOperationResultCode {
 
 #[derive(TryFromPrimitive)]
 #[repr(u64)]
+pub enum FetchResultPollResultCode {
+    Ok = 0,
+    FailedToAccessMemory = 1,
+    ResultAddrOutOfMemoryBounds = 2,
+    InternalRuntimeAssertionError = 3,
+}
+
+#[derive(TryFromPrimitive)]
+#[repr(u64)]
 pub enum FetchResultSerializeResultCode {
     Ok = 0,
     FailedToAccessMemory = 1,
